50c3d3d2a4a543365165e0f5a3ef58532a757ba5
vaser888 <vazer888@gmail.com>
2019-10-17 21:07:58 -0400

    updates

--- engineering_cheat_sheet/engineering_cheat_sheet.js
+++ engineering_cheat_sheet/engineering_cheat_sheet.js
@@ -4,59 +4,68 @@ window.onload = function(event){
 
 resistorType.addEventListener("change", displaySelections);
 
-firstBand.addEventListener("change", updateValueFourBands);
-secondBand.addEventListener("change", updateValueFourBands);
-thirdBand.addEventListener("change", updateValueFourBands);
-forthBand.addEventListener("change", updateValueFourBands);
+firstBandBox.addEventListener("change", calculateValueOfBands);
+secondBandBox.addEventListener("change", calculateValueOfBands);
+thirdBandBox.addEventListener("change", calculateValueOfBands);
+multiplierBandBox.addEventListener("change", calculateValueOfBands);
+toloranceBandBox.addEventListener("change", calculateValueOfBands);
 
 function displaySelections(){
 	var al = document.querySelector("#resistorType").value
-	var off_4 = document.getElementById("fourBandResistor").className = "BandResistor_Hidden";
-	var off_5 = document.getElementById("fiveBandResistor").className = "BandResistor_Hidden";
-	var off_6 = document.getElementById("sixBandResistor").className = "BandResistor_Hidden";
+	var off_4 = document.getElementById("resistorBands").className = "bandResistor_Hidden";
 	if (al === "0"){
 		off_4;
-		off_5;
-		off_6;
 	}
 	else if (al === "4"){
-		off_5;
-		off_6;
-		document.getElementById("fourBandResistor").className = "BandResistor_Visible"; 
+		document.getElementById("resistorBands").className = "bandResistor_Visible"; 
+		document.getElementById("thirdBandBox").className = "bandResistor_Hidden";
+		document.getElementById("tempBandBox").className = "bandResistor_Hidden";
+		document.getElementById("multiplierBandTitleChange").textContent = "3rd Band (Multiplier)";
+		document.getElementById("toloranceBandTitleChange").textContent = "4th Band (Tolorance)";
 	}
 	else if (al === "5"){
-		off_4;
-		off_6;
-		document.getElementById("fiveBandResistor").className = "BandResistor_Visible";
+		document.getElementById("resistorBands").className = "bandResistor_Visible"; 
+		document.getElementById("thirdBandBox").className = "bandResistor_Visible";
+		document.getElementById("tempBandBox").className = "bandResistor_Hidden";
+		document.getElementById("multiplierBandTitleChange").textContent = "4th Band (Multiplier)";
+		document.getElementById("toloranceBandTitleChange").textContent = "5th Band (Tolorance)";
 	}
 	else if (al === "6"){
-		off_5;
-		off_6;
-		document.getElementById("sixBandResistor").className = "BandResistor_Visible";
+		document.getElementById("resistorBands").className = "bandResistor_Visible"; 
+		document.getElementById("thirdBandBox").className = "bandResistor_Visible";
+		document.getElementById("tempBandBox").className = "bandResistor_Visible";
 	}
 }
 
-function updateValueFourBands() {
-	var fir = document.querySelector("#firstBand").value;
-	var sec = document.querySelector("#secondBand").value;
-	var thr = document.querySelector("#thirdBand").value;
-	var fou = document.querySelector("#forthBand").value;
+function calculateValueOfBands() {
+	var fir = document.querySelector("#firstBandBox").value;
+	var sec = document.querySelector("#secondBandBox").value;
+	var thr = document.querySelector("#thirdBandBox").value;
+	var fou = document.querySelector("#multiplierBandBox").value;
+	var fiv = document.querySelector("#toloranceBandBox").value;
 	
-	var ohmResult = (fir + sec) *thr ;
+	
+	var al = document.querySelector("#resistorType").value
+		if (al === "4") {
+			var ohmResult = (fir + sec) * fou;
+		}
+		if (al === "5" || al === "6"){
+			var ohmResult = (fir + sec + thr) * fou;
+		}
+	
 	var kohmResult = ohmResult/1000;
 	var mohmResult = kohmResult/1000;
-	var per = fou * 100;
-	var rangeValue = ohmResult * fou;
+	var per = fiv * 100;
+	var rangeValue = ohmResult * fiv;
 	var lowRangeOhmResult = ohmResult - rangeValue;
 	var highRangeOhmResult = ohmResult + rangeValue;
 	var lowRangeKOhmResult = lowRangeOhmResult/1000;
 	var highRangeKOhmResult = highRangeOhmResult/1000;
 	var lowRangeMOhmResult = lowRangeKOhmResult/1000;
 	var highRangeMOhmResult = highRangeKOhmResult/1000;
-
 	
-	document.querySelector(".resistorResultOne").textContent = "Ohms Result = " + ohmResult +" ohms " +  per + "%";
-	document.querySelector(".resistorResultTwo").textContent = "k Ohms Result = " + kohmResult + "k ohms " + per + "%"; 
+	document.querySelector(".resistorResultOne").textContent = "Ohms Result = " + ohmResult +" \u2126 \u00b1 " +  per + "%";
+	document.querySelector(".resistorResultTwo").textContent = "k Ohms Result = " + kohmResult + " k\u2126 +/- " + per + "%"; 
 	document.querySelector(".resistorResultThree").textContent = "M Ohms Result = " + mohmResult + "M ohms " + per + "%";
 	
 	document.querySelector(".resistorRangeResultOne").textContent = "Ohms Range = " + lowRangeOhmResult + " ohms - " + highRangeOhmResult + " ohms";
