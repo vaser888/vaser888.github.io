d88fbaed990023d78cf5a50f21dbbdb8833f0dab
vaser888 <vazer888@gmail.com>
2019-10-31 10:48:27 -0400

    update

--- whats_for_lunch/whats_for_lunch.js
+++ whats_for_lunch/whats_for_lunch.js
@@ -3,19 +3,66 @@ document.querySelector(".selectAllOne").onclick = function selected (){
 		//alert("hello");
 	if (document.querySelector(".selectAllOne:checked")){
 		//alert("hi");
-		var makeList = document.querySelectorAll(".restaurant");
-		//console.log(makeList);
-		al = Array.from(makeList);
-		for (ab = 0; ab<9; ++ab){
+		makeList();
+		for (ab = 0; ab<11; ++ab){
 			console.log(al[ab]);
 			al[ab].checked = true;
 		}
 	}
 	else if (document.querySelector(".selectAllOne")){
-		var makeList = document.querySelectorAll(".restaurant:checked");
-		//console.log(makeList);
-		al = Array.from(makeList);
-		for (ab = 0; ab<9; ++ab){
+		makeList();
+		for (ab = 0; ab<11; ++ab){
+			console.log(al[ab]);
+			al[ab].checked = false;
+		}
+	}
+}
+
+document.querySelector(".selectAllTwo").onclick = function selected (){
+	if (document.querySelector(".selectAllTwo:checked")){
+		makeList();
+		for (ab = 11; ab<17; ++ab){
+			console.log(al[ab]);
+			al[ab].checked = true;
+		}
+	}
+	else if (document.querySelector(".selectAllTwo")){
+		makeList();
+		for (ab = 11; ab<17; ++ab){
+			console.log(al[ab]);
+			al[ab].checked = false;
+		}
+	}
+}
+
+document.querySelector(".selectAllThree").onclick = function selected (){
+	if (document.querySelector(".selectAllThree:checked")){
+		makeList();
+		for (ab = 17; ab<24; ++ab){
+			console.log(al[ab]);
+			al[ab].checked = true;
+		}
+	}
+	else if (document.querySelector(".selectAllThree")){
+		makeList();
+		for (ab = 17; ab<24; ++ab){
+			console.log(al[ab]);
+			al[ab].checked = false;
+		}
+	}
+}
+
+document.querySelector(".selectAllFour").onclick = function selected (){
+	if (document.querySelector(".selectAllFour:checked")){
+		makeList();
+		for (ab = 24; ab<26; ++ab){
+			console.log(al[ab]);
+			al[ab].checked = true;
+		}
+	}
+	else if (document.querySelector(".selectAllFour")){
+		makeList();
+		for (ab = 24; ab<26; ++ab){
 			console.log(al[ab]);
 			al[ab].checked = false;
 		}
@@ -25,6 +72,12 @@ document.querySelector(".selectAllOne").onclick = function selected (){
 
 
 
+function makeList(){
+		var li = document.querySelectorAll(".restaurant");
+		//console.log(makeList);
+		al = Array.from(li);
+		return al;
+		}
 
 document.querySelector(".pickLunch").onclick = function pick (){
 	var checkList = document.querySelectorAll(".restaurant:checked");
