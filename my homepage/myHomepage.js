1495e847cb7d3b1ee003623ae0de6953a155e048
vaser888 <52551019+vaser888@users.noreply.github.com>
2020-02-12 23:38:20 -0500

    my webpage

---
+++ my homepage/myHomepage.js
@@ -0,0 +1,102 @@
+
+
+var names = ["vaser888", "TechPony", "lollipony", "TheFloatingTree", "PennyWren", "VanillaGhosties",
+"aemantaslim", "JeNnDyLyOn", "RenardeLouve", "stratodraw", "SPW", "AnderDragon"];
+var numberOfUsers = names.length-1;
+
+
+// searchs for streams that are live and display any that are live 
+for (i=0; i<= numberOfUsers; i++) {
+
+	var xmlhttp = new XMLHttpRequest();
+	xmlhttp.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) {
+			var myObj = JSON.parse(this.responseText);
+			//document.getElementById("test").innerHTML = myObj.user_id;
+			//document.getElementById("help").src = t;
+			var thumbnail = myObj.thumbnails.web;
+			var artistName = myObj.name;
+			var onlineTest = myObj.online;
+			
+			if (onlineTest === true){
+
+				var linkElement = document.createElement('a');
+				linkElement.href = "https://picarto.tv/" + artistName;
+
+				var image = document.createElement("img");
+				image.setAttribute("src", thumbnail);
+				image.setAttribute("style", "width:300px; display: block; margin-left: auto;margin-right: auto");
+				linkElement.appendChild(image);
+
+				document.getElementById("rightColumn").appendChild(linkElement);
+
+
+				var para = document.createElement('p');
+				para.innerHTML = '<center><a href= https://picarto.tv/' + artistName + ' title="http://example.com">'+artistName+'</a>';
+				document.getElementById("rightColumn").appendChild(para);				
+			}
+		}		  
+	};
+	xmlhttp.open("GET", "https://api.picarto.tv/v1/channel/name/" + names[i], true);
+	xmlhttp.send();
+}
+
+// get previous game NHL data and display it
+var xmlhttp = new XMLHttpRequest();
+	xmlhttp.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) {
+			var myObj = JSON.parse(this.responseText);
+			//document.getElementById("previousGame").innerHTML = myObj.teams[0].previousGameSchedule.dates[0].games[0].teams.home.team.name;
+			//var prevHome = myObj.teams[0].previousGameSchedule.dates[0].games[0].teams.home.team.name;
+			//var prevAway = myObj.teams[0].previousGameSchedule.dates[0].games[0].teams.away.team.name;
+			//var prevHomeScore = myObj.teams[0].previousGameSchedule.dates[0].games[0].teams.home.score;
+			//var prevAwayScore = myObj.teams[0].previousGameSchedule.dates[0].games[0].teams.away.score;
+			
+			//var para = document.createElement('p');
+			//para.innerHTML = prevHome + " VS. " + prevAway + "<br>" + 
+			//prevHomeScore + " " + prevAwayScore;
+			//document.getElementById("previousGame").appendChild(para);
+			
+			document.getElementById("previousHome").innerHTML = myObj.teams[0].previousGameSchedule.dates[0].games[0].teams.home.team.name;
+			document.getElementById("previousAway").innerHTML = myObj.teams[0].previousGameSchedule.dates[0].games[0].teams.away.team.name;
+			document.getElementById("previousHomeScore").innerHTML = myObj.teams[0].previousGameSchedule.dates[0].games[0].teams.home.score;
+			document.getElementById("previousAwayScore").innerHTML = myObj.teams[0].previousGameSchedule.dates[0].games[0].teams.away.score;
+			
+			
+		}
+	};
+xmlhttp.open("GET", "https://statsapi.web.nhl.com/api/v1/teams/8?expand=team.schedule.previous" , true);
+xmlhttp.send();
+
+
+
+var xmlhttp = new XMLHttpRequest();
+	xmlhttp.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) {
+			var myObj = JSON.parse(this.responseText);
+			var currentGameId = myObj.teams[0].nextGameSchedule.dates[0].games[0].gamePk;
+			document.getElementById("currentHome").innerHTML = myObj.teams[0].nextGameSchedule.dates[0].games[0].gamePk;
+			document.getElementById("nextHome").innerHTML = myObj.teams[0].nextGameSchedule.dates[0].games[0].teams.home.team.name;
+			document.getElementById("nextAway").innerHTML = myObj.teams[0].nextGameSchedule.dates[0].games[0].teams.away.team.name;
+
+		}
+
+	};
+xmlhttp.open("GET", "https://statsapi.web.nhl.com/api/v1/teams/8?expand=team.schedule.next" , true);
+xmlhttp.send();
+
+var t = document.getElementById("currentHome").value;
+console.log(t);
+
+/* var xmlhttp = new XMLHttpRequest();
+	xmlhttp.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) {
+			var myObj = JSON.parse(this.responseText);
+			console.log("hello");
+			
+		}
+
+	};
+xmlhttp.open("GET", "https://statsapi.web.nhl.com/api/v1/game/"+  +"/feed/live" , true);
+xmlhttp.send(); */
+
