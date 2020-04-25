712caa2129d45ebf40905f1bfd5794788dc539a6
vaser888 <52551019+vaser888@users.noreply.github.com>
2020-04-25 01:54:08 -0400

    update

---
+++ improvedDerpi/improvedDerpi.js
@@ -0,0 +1,7 @@
+
+document.getElementById("randomButton").addEventListener("click", (event)=>{
+    alert("hi");
+});
+document.getElementById("realDerpiButton").addEventListener("click", (event)=>{
+    window.location.href = "https://derpibooru.org/";
+});
