96a997dad6bc93cb21a4d7946cf8712bbb4de3f2
vaser888 <52551019+vaser888@users.noreply.github.com>
2021-01-16 20:44:06 -0500

    js update

--- my_homepage/myHomepage.js
+++ my_homepage/myHomepage.js
@@ -80,13 +80,19 @@ var xmlhttp = new XMLHttpRequest();
 			//para.innerHTML = prevHome + " VS. " + prevAway + "<br>" + 
 			//prevHomeScore + " " + prevAwayScore;
 			//document.getElementById("previousGame").appendChild(para);
-			
+			/*
 			document.getElementById("previousHome").innerHTML = myObj.teams[0].previousGameSchedule.dates[0].games[0].teams.home.team.name;
 			document.getElementById("previousAway").innerHTML = myObj.teams[0].previousGameSchedule.dates[0].games[0].teams.away.team.name;
 			document.getElementById("previousHomeScore").innerHTML = myObj.teams[0].previousGameSchedule.dates[0].games[0].teams.home.score;
 			document.getElementById("previousAwayScore").innerHTML = myObj.teams[0].previousGameSchedule.dates[0].games[0].teams.away.score;
-			
-			
+			*/
+
+			var a = myObj.teams[0].previousGameSchedule.dates[0].games[0].teams.home.team.name;
+			var b = myObj.teams[0].previousGameSchedule.dates[0].games[0].teams.away.team.name;
+			var c = myObj.teams[0].previousGameSchedule.dates[0].games[0].teams.home.score;
+			var d = myObj.teams[0].previousGameSchedule.dates[0].games[0].teams.away.score;
+			var i = getLastGameInfo(a,b,c,d);
+			makeTable(i,3,"lastGame");
 		}
 	};
 xmlhttp.open("GET", "https://statsapi.web.nhl.com/api/v1/teams/8?expand=team.schedule.previous" , true);
@@ -108,27 +114,216 @@ xmlhttp.open("GET", "https://statsapi.web.nhl.com/api/v1/teams/8?expand=team.sch
 xmlhttp.send(); */
 
 fetch("https://statsapi.web.nhl.com/api/v1/teams/8?expand=team.schedule.next").then(function (r) { return r.json() }).then(function (json0) {
-	//console.log(json);
-	document.getElementById("nextGameDate").innerHTML = "Next game: " + json0.teams[0].nextGameSchedule.dates[0].date;
-	document.getElementById("nextHome").innerHTML =json0.teams[0].nextGameSchedule.dates[0].games[0].teams.home.team.name;
-	document.getElementById("nextAway").innerHTML =json0.teams[0].nextGameSchedule.dates[0].games[0].teams.away.team.name;
+	//console.log(json0);
+	var w = "At " + json0.teams[0].nextGameSchedule.dates[0].games[0].venue.name;
+	var utcDate = json0.teams[0].nextGameSchedule.dates[0].games[0].gameDate;
+	var localDate = new Date(utcDate);
+
+	var x = "Next game: " + localDate.toLocaleString("en-US", {hour:"numeric", hour12:true,year:"numeric", month:"numeric", day:"numeric"});//use can also use Long instead of numeric
+	var y = innerHTML =json0.teams[0].nextGameSchedule.dates[0].games[0].teams.home.team.name;
+	var z =json0.teams[0].nextGameSchedule.dates[0].games[0].teams.away.team.name;
+	//document.getElementById("nextGameDate").innerHTML = "Next game: " + json0.teams[0].nextGameSchedule.dates[0].date;
+	var i = getNextGameInfo(x,y,z,w);
+	makeTable(i,3,"nextGame");
+	//document.getElementById("nextHome").innerHTML =json0.teams[0].nextGameSchedule.dates[0].games[0].teams.home.team.name;
+	//document.getElementById("nextAway").innerHTML =json0.teams[0].nextGameSchedule.dates[0].games[0].teams.away.team.name;
 	var testj = json0.teams[0].nextGameSchedule.dates[0].games[0].gamePk;
 	fetch("https://statsapi.web.nhl.com/api/v1/game/"+testj+"/feed/live").then(function (r) { return r.json() }).then(function (json1) {
-		console.log(json1);
+		//console.log(json1);
+		var testLive = json1.liveData.plays.currentPlay.about.period;
+		if (testLive != "0"){
+			//console.log(testLive);
+			var a = json1.gameData.teams.home.abbreviation;
+			var b = json1.gameData.teams.away.abbreviation;
+			var c = json1.liveData.boxscore.teams.home.teamStats.teamSkaterStats.goals;
+			var d = json1.liveData.boxscore.teams.away.teamStats.teamSkaterStats.goals;
+			var e = json1.liveData.linescore.teams.home.shotsOnGoal;
+			var f = json1.liveData.linescore.teams.away.shotsOnGoal;
+			var i = getCurrentGameInfo(a,b,c,d,e,f);
+			makeTable(i,3,"currentGame");
+		}
+		/*
 		document.getElementById("currentAway").innerHTML = json1.gameData.teams.away.abbreviation;
 		document.getElementById("currentHome").innerHTML = json1.gameData.teams.home.abbreviation;
 		document.getElementById("currentAwayScore").innerHTML = json1.liveData.boxscore.teams.away.teamStats.teamSkaterStats.goals;
 		document.getElementById("currentHomeScore").innerHTML = json1.liveData.boxscore.teams.home.teamStats.teamSkaterStats.goals;
 		document.getElementById("currentAwayShots").innerHTML = json1.liveData.linescore.teams.away.shotsOnGoal;
 		document.getElementById("currentHomeShots").innerHTML = json1.liveData.linescore.teams.home.shotsOnGoal;	
+		*/
 	})
 
 })
 
-
+//hover over stream image to show gif 
 function hide(x){
 	x.style.opacity = "0";
 }
 function unhide(x){
 	x.style.opacity = "1";
 }
+
+/////
+
+function generateNextGameInfo(date,homeTeam,awayTeam,venue) {
+	var div1 = document.createElement("div");
+	var table1 = document.createElement("table");
+	var tbody1 = document.createElement("tbody");
+	var tr1 = document.createElement("tr");
+	var tr2 = document.createElement("tr");
+	var tr3 = document.createElement("tr");
+	var td1 = document.createElement("td");
+	var td2 = document.createElement("td");
+	var td3 = document.createElement("td");
+	var td4 = document.createElement("td");
+	var td5 = document.createElement("td");
+	td1.innerHTML = date;
+	td2.innerHTML = homeTeam;
+	td3.innerHTML = "Vs.";
+	td4.innerHTML = awayTeam;
+	td5.innerHTML = venue;
+	div1.setAttribute("class", "game1");
+	td1.setAttribute("colspan", "3");
+	td5.setAttribute("colspan", "3");
+	tr1.appendChild(td1);
+	tbody1.appendChild(tr1);
+	tr2.appendChild(td2);
+	tr2.appendChild(td3);
+	tr2.appendChild(td4);
+	tr3.appendChild(td5);
+	tbody1.appendChild(tr2);
+	tbody1.appendChild(tr3); 
+	table1.appendChild(tbody1);
+	div1.appendChild(table1);
+	document.getElementById("nextGame").appendChild(div1);
+}
+
+function generateNextGameInfoV2(date,homeTeam,awayTeam,venue) {
+	var div1 = document.createElement("div");
+	var table1 = document.createElement("table");
+	var tbody1 = document.createElement("tbody");
+	var a = [venue];
+	var tr1 = gameTableMakeTr1(3,date);
+	var tr2 = gameTableMakeTr2(0,homeTeam,awayTeam);
+	var tr3 = gameTableMakeTr3(1,3,a);
+	div1.setAttribute("class", "game");
+	tbody1.appendChild(tr1);
+	tbody1.appendChild(tr2);
+	tbody1.appendChild(tr3);
+	table1.appendChild(tbody1);
+	div1.appendChild(table1);
+	document.getElementById("nextGame").appendChild(div1);
+}
+
+function generatePreviousGameInfo (homeTeam,awayTeam,homeScore,awayScore) {
+	var div1 = document.createElement("div");
+	var table1 = document.createElement("table");
+	var tbody1 = document.createElement("tbody");
+	var a = [homeScore,"",awayScore]
+	var tr1 = gameTableMakeTr1(3,"Last game");
+	var tr2 = gameTableMakeTr2(0,homeTeam,awayTeam);
+	var tr3 = gameTableMakeTr3(3,0,a);
+	tbody1.appendChild(tr1);
+	tbody1.appendChild(tr2);
+	tbody1.appendChild(tr3);
+	table1.appendChild(tbody1);
+	div1.appendChild(table1);
+	document.getElementById("lastGame").appendChild(div1);
+}
+
+function getTheThings() {
+	var a= ["test","", "lol"]
+	return [
+		gameTableMakeTr1(3,"Last game"),
+		gameTableMakeTr2(0,"mtl","bos"),
+		gameTableMakeTr3(3,0,a)
+	];
+}
+
+
+/////////
+
+
+function getNextGameInfo(date,homeTeam,awayTeam,venue) {
+	var a= [venue];
+	return [
+		gameTableMakeTr1(3,date),
+		gameTableMakeTr2(0,homeTeam,awayTeam),
+		gameTableMakeTr3(1,3,a)
+	];
+}
+
+function getLastGameInfo(homeTeam,awayTeam,homeScore,awayScore) {
+	var a= [homeScore,"",awayScore];
+	return [
+		gameTableMakeTr1(3,"Last game"),
+		gameTableMakeTr2(0,homeTeam,awayTeam),
+		gameTableMakeTr3(3,0,a)
+	];
+}
+
+function getCurrentGameInfo(homeTeam,awayTeam,homeScore,awayScore,homeShots,awayShots) {
+	var a= [homeShots,homeScore,"",awayScore,awayShots];
+	return [
+		gameTableMakeTr1(5,"Current game"),
+		gameTableMakeTr2(2,homeTeam,awayTeam),
+		gameTableMakeTr3(5,0,a)
+	];
+}
+
+function testGen () {
+	var i = getTheThings();
+	testGen(i,3,"infoArea");
+}
+
+function makeTable(tdArray,tdLength,elementId) {
+	var div1 = document.createElement("div");
+	var table1 = document.createElement("table");
+	var tbody1 = document.createElement("tbody");
+	div1.setAttribute("class", "game");
+	for (r=0; r<=(tdLength-1); r++) {
+		tbody1.appendChild(tdArray[r]);
+	}
+	table1.appendChild(tbody1);
+	div1.appendChild(table1);
+	document.getElementById(elementId).appendChild(div1);
+}
+
+function gameTableMakeTr1(colspanNumber, title) {
+	var tr1 = document.createElement("tr");
+	var td1 = document.createElement("td");
+	td1.innerHTML = title;
+	td1.setAttribute("colspan", colspanNumber);
+	tr1.appendChild(td1);
+	return tr1;
+}
+
+function gameTableMakeTr2(colspanNumberSides, homeTeam, awayTeam){
+	var tr2 = document.createElement("tr");
+	var td1 = document.createElement("td");
+	var td2 = document.createElement("td");
+	var td3 = document.createElement("td");
+	td1.innerHTML = homeTeam;
+	td2.innerHTML = "Vs.";
+	td3.innerHTML = awayTeam;
+	td1.setAttribute("colspan", colspanNumberSides);
+	td3.setAttribute("colspan", colspanNumberSides);
+	tr2.appendChild(td1);
+	tr2.appendChild(td2);
+	tr2.appendChild(td3);
+	return tr2;
+}
+
+function gameTableMakeTr3(numberOfCol, colspanNumber, dataArray) {
+	var tr3 = document.createElement("tr");
+	for (i=0; i<=(numberOfCol-1); i++){
+
+		var td = document.createElement("td");
+		if (numberOfCol == "1"){
+			td.setAttribute("colspan", colspanNumber);
+		}		
+		td.innerHTML = dataArray[i];
+		tr3.appendChild(td);
+	}
+	return tr3;
+}
+
